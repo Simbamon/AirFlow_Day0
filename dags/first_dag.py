ac85cb3628170040cf0a17a537b086310c66a367
Simbamon <sbhan9511@gmail.com>
2023-03-04 18:52:38 +0900

    task dependency methods updated

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -11,7 +11,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='first_dag_v2',
+    dag_id='first_dag_v5',
     default_args=default_args,
     description='This is our first dag that we wrote',
     start_date=datetime(2023, 3, 3, 11),
@@ -27,4 +27,18 @@ with DAG(
         bash_command="echo hello, this is to test out the second task"
     )
 
-    task1.set_downstream(task2)
+    task3 = BashOperator(
+        task_id='third_task',
+        bash_command="echo hello, Im task 3 and will be running after task 1 at the same time as task 2"
+    )
+
+    # Task dependency method 1
+    # task1.set_downstream(task2)
+    # task1.set_downstream(task3)
+
+    # Task dependency method 2
+    # task1 >> task2
+    # task1 >> task3
+
+    # Task dependency method 3
+    task1 >> [task2, task3]
