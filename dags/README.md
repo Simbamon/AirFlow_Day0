70deb601ae6fff6769a98ffd574a512c69a0cfd1
Simbamon <sbhan9511@gmail.com>
2023-03-05 16:42:27 +0900

    xcoms feature added

---
+++ dags/README.md
@@ -0,0 +1,4 @@
+# Apache Airflow Notes
+
+xcoms maximum size is only 48kb
+Never use xcoms to share large data such as pandas dataframe
--- dags/create_python_operator_dag.py
+++ dags/create_python_operator_dag.py
@@ -9,21 +9,42 @@ default_args = {
     'retry_delay': timedelta(minutes=2)
 }
 
-
-def greet(name, age):
+# ti is task instance
+def greet(ti):
+    first_name = ti.xcom_pull(task_ids='get_name_task', key='first_name')
+    last_name = ti.xcom_pull(task_ids='get_name_task', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age_task', key='age')
     print("Hello Python!\n")
-    print(f"my name is {name}, " f"and I am {age} years old")
+    print(f"my name is {first_name} {last_name}, " f"and I am {age} years old")
+
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Mike')
+    ti.xcom_push(key='last_name', value='Thomson')
+
+def get_age(ti):
+    ti.xcom_push(key='age', value=23)
+
 with DAG(
     default_args=default_args,
-    dag_id='dag_with_python_operator_v02',
+    dag_id='dag_with_python_operator_v06',
     description='Our first dag using python operator',
-    start_date=datetime(2023, 3, 3),
+    start_date=datetime(2023, 3, 4),
     schedule_interval='@daily',
 ) as dag:
     task1 = PythonOperator(
         task_id='greet_with_python',
         python_callable=greet,
-        op_kwargs={'name': 'Jesse', 'age': 23}
+        # op_kwargs={'age': 23}
     )
 
-    task1
+    task2 = PythonOperator(
+        task_id='get_name_task',
+        python_callable=get_name,
+    )
+
+    task3 = PythonOperator(
+        task_id='get_age_task',
+        python_callable=get_age,
+    )
+
+    [task2, task3] >> task1
