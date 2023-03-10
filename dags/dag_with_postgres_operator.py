3392c068a3c0a731f7c939d225e557a283039efb
Simbamon <sbhan9511@gmail.com>
2023-03-10 18:43:10 +0900

    postgres dag updated

--- dags/dag_with_postgres_operator.py
+++ dags/dag_with_postgres_operator.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='dag_with_postgres_operator_v01',
+    dag_id='dag_with_postgres_operator_v03',
     default_args=default_args,
     start_date=datetime(2023, 3, 3),
     schedule_interval='0 0 * * *'
@@ -26,4 +26,28 @@ with DAG(
             )
         """
     )
-    task1
+
+    task2 = PostgresOperator(
+        task_id='insert_into_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            insert into dag_runs(
+                dt,
+                dag_id
+            )
+            values (
+            '{{ds}}',
+            '{{dag.dag_id}}'
+            )
+        """
+    )
+
+    task3 = PostgresOperator(
+        task_id='delete_data_from_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            delete from dag_runs
+            where dt = '{{ds}}' and dag_id =  '{{dag.dag_id}}'
+        """
+    )
+    task1 >> task3 >> task2
